b6aa8dfbcb925c643a922bd73ece56477e5c8173
kadesh sg <kadeshsg@gmail.com>
2024-02-04 20:19:44 +0530

    adding commit to test

--- src/main/java/com/productservices/productservices/ProductservicesApplication.java
+++ src/main/java/com/productservices/productservices/ProductservicesApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProductservicesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProductservicesApplication.class, args); 
+		SpringApplication.run(ProductservicesApplication.class, args);
+		System.out.println("kadesh");
 	}
 
 }
